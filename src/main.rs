66b2c4529a898040a6b4c642f3461e511caedbff
Bart Massey <bart.massey@gmail.com>
2023-08-01 16:59:28 -0700

    fixed clippy warnings

--- src/main.rs
+++ src/main.rs
@@ -56,11 +56,11 @@ fn note_to_freq() -> f32 {
 
         let semitone_bend = 2.0 * (pitch_bend);
 
-        let new_freq = 440.0 * libm::powf(
+        // New frequency.
+        440.0 * libm::powf(
             2.0,
             (1.0 / 12.0) * (note as f32  + semitone_bend) + octave as f32,
-        );
-        new_freq
+        )
     })
 }
 
@@ -75,10 +75,9 @@ fn steps_to_note()  {
             *OCTAVE.borrow(cs).borrow_mut() = octave;
             let steps = steps % 7;
             let is_minor_scale =  *USE_MINOR_SCALE.borrow(cs).borrow();
-            let mut note = 0;
-            if is_minor_scale {
+            let note = if is_minor_scale {
                 // minor
-                note = match steps {
+                match steps {
                     0 => 0,  // root
                     1 => 2,  // 2nd: whole step from root
                     2 => 3,  // 3rd: half step from 2nd
@@ -88,10 +87,10 @@ fn steps_to_note()  {
                     6 => 10, // 7th: whole step from 6th
                     7 => 12, // octave: whole step from 7th
                     _ => panic!("Invalid scale step"),
-                };
+                }
             } else {
                 // major
-                note = match steps {
+                match steps {
                     0 => 0,  // root
                     1 => 2,  // 2nd: whole step from root
                     2 => 4,  // 3rd: whole step from 2nd
@@ -101,8 +100,8 @@ fn steps_to_note()  {
                     6 => 11, // 7th: whole step from 6th
                     7 => 12, // octave: half step from 7th
                     _ => panic!("Invalid scale step"),
-                };
-            }
+                }
+            };
 
             *NOTE.borrow(cs).borrow_mut() = note;
         }
@@ -160,101 +159,98 @@ fn RTC0() {
 fn main() -> ! {
     rtt_init_print!();
 
-    if let Some(mut board) = Board::take() {
-        let gpiote = Gpiote::new(board.GPIOTE);
+    let mut board = Board::take().unwrap();
+    let gpiote = Gpiote::new(board.GPIOTE);
 
-        let channel0 = gpiote.channel0();
-        channel0
-            .input_pin(&board.buttons.button_a.degrade())
-            .hi_to_lo()
-            .enable_interrupt();
-        channel0.reset_events();
+    let channel0 = gpiote.channel0();
+    channel0
+        .input_pin(&board.buttons.button_a.degrade())
+        .hi_to_lo()
+        .enable_interrupt();
+    channel0.reset_events();
 
-        let channel1 = gpiote.channel1();
-        channel1
-            .input_pin(&board.buttons.button_b.degrade())
-            .hi_to_lo()
-            .enable_interrupt();
-        channel1.reset_events();
+    let channel1 = gpiote.channel1();
+    channel1
+        .input_pin(&board.buttons.button_b.degrade())
+        .hi_to_lo()
+        .enable_interrupt();
+    channel1.reset_events();
 
-        let mut i2c =
-            { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
+    let mut i2c =
+        { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
 
-        let mut acc = [0];
+    let mut acc = [0];
 
-        i2c.write_read(ACCELEROMETER_ADDR, &[ACCELEROMETER_ID_REG], &mut acc)
-            .unwrap();
+    i2c.write_read(ACCELEROMETER_ADDR, &[ACCELEROMETER_ID_REG], &mut acc)
+        .unwrap();
 
-        rprintln!("The accelerometer chip's id is: {:#b}", acc[0]);
+    rprintln!("The accelerometer chip's id is: {:#b}", acc[0]);
 
-        let mut sensor = Lsm303agr::new_with_i2c(i2c);
-        sensor.init().unwrap();
-        sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
+    let mut sensor = Lsm303agr::new_with_i2c(i2c);
+    sensor.init().unwrap();
+    sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
 
-        cortex_m::interrupt::free(move |cs| {
-            /* Enable external GPIO interrupts */
-            unsafe {
-                pac::NVIC::unmask(pac::Interrupt::GPIOTE);
-            }
-            pac::NVIC::unpend(pac::Interrupt::GPIOTE);
+    cortex_m::interrupt::free(move |cs| {
+        /* Enable external GPIO interrupts */
+        unsafe {
+            pac::NVIC::unmask(pac::Interrupt::GPIOTE);
+        }
+        pac::NVIC::unpend(pac::Interrupt::GPIOTE);
 
-            *GPIO.borrow(cs).borrow_mut() = Some(ButtonContext::new(gpiote));
+        *GPIO.borrow(cs).borrow_mut() = Some(ButtonContext::new(gpiote));
 
-            let mut rtc = Rtc::new(board.RTC0, 511).unwrap();
-            rtc.enable_counter();
-            rtc.enable_interrupt(RtcInterrupt::Tick, Some(&mut board.NVIC));
-            rtc.enable_event(RtcInterrupt::Tick);
+        let mut rtc = Rtc::new(board.RTC0, 511).unwrap();
+        rtc.enable_counter();
+        rtc.enable_interrupt(RtcInterrupt::Tick, Some(&mut board.NVIC));
+        rtc.enable_event(RtcInterrupt::Tick);
 
-            *RTC.borrow(cs).borrow_mut() = Some(rtc);
+        *RTC.borrow(cs).borrow_mut() = Some(rtc);
 
-            let mut speaker_pin = board.speaker_pin.into_push_pull_output(gpio::Level::High);
-            let _ = speaker_pin.set_low();
+        let mut speaker_pin = board.speaker_pin.into_push_pull_output(gpio::Level::High);
+        let _ = speaker_pin.set_low();
 
-            // Use the PWM peripheral to generate a waveform for the speaker
-            let speaker = pwm::Pwm::new(board.PWM0);
-            speaker
-                // output the waveform on the speaker pin
-                .set_output_pin(pwm::Channel::C0, speaker_pin.degrade())
-                // Use prescale by 16 to achive darker sounds
-                .set_prescaler(pwm::Prescaler::Div16)
-                // Initial frequency
-                .set_period(Hertz(440u32))
-                // Configure for up and down counter mode
-                .set_counter_mode(pwm::CounterMode::UpAndDown)
-                // Set maximum duty cycle
-                .set_max_duty(5000)
-                // enable PWM
-                .enable();
+        // Use the PWM peripheral to generate a waveform for the speaker
+        let speaker = pwm::Pwm::new(board.PWM0);
+        speaker
+            // output the waveform on the speaker pin
+            .set_output_pin(pwm::Channel::C0, speaker_pin.degrade())
+            // Use prescale by 16 to achive darker sounds
+            .set_prescaler(pwm::Prescaler::Div16)
+            // Initial frequency
+            .set_period(Hertz(440u32))
+            // Configure for up and down counter mode
+            .set_counter_mode(pwm::CounterMode::UpAndDown)
+            // Set maximum duty cycle
+            .set_max_duty(5000)
+            // enable PWM
+            .enable();
 
-            // Configure 50% duty cycle
-            let max_duty = speaker.max_duty();
-            speaker.set_duty_on_common(max_duty / 2);
+        // Configure 50% duty cycle
+        let max_duty = speaker.max_duty();
+        speaker.set_duty_on_common(max_duty / 2);
 
-            *SPEAKER.borrow(cs).borrow_mut() = Some(speaker);
+        *SPEAKER.borrow(cs).borrow_mut() = Some(speaker);
 
-            unsafe {
-                pac::NVIC::unmask(pac::Interrupt::RTC0);
-            }
-            pac::NVIC::unpend(pac::Interrupt::RTC0);
-        });
+        unsafe {
+            pac::NVIC::unmask(pac::Interrupt::RTC0);
+        }
+        pac::NVIC::unpend(pac::Interrupt::RTC0);
+    });
 
-        loop {
-            if sensor.accel_status().unwrap().xyz_new_data {
-                let data = sensor.accel_data().unwrap();
+    loop {
+        if sensor.accel_status().unwrap().xyz_new_data {
+            let data = sensor.accel_data().unwrap();
 
-                rprintln!("Acceleration: x {} y {} z {}", data.x, data.y, data.z);
-                cortex_m::interrupt::free(move |cs| {
-                    // Standard midi mapping ish
-                    let new_freq = data.x as f32 / 8192.0;
-                    let abs_data = libm::fabsf(data.x as f32);
-                    // Extremely scientific method to arrive at 100.
-                    if abs_data > 100. {
-                        *PITCH_BEND.borrow(cs).borrow_mut() = new_freq ;
-                    }
-                });
-            }
+            rprintln!("Acceleration: x {} y {} z {}", data.x, data.y, data.z);
+            cortex_m::interrupt::free(move |cs| {
+                // Standard midi mapping ish
+                let new_freq = data.x as f32 / 8192.0;
+                let abs_data = libm::fabsf(data.x as f32);
+                // Extremely scientific method to arrive at 100.
+                if abs_data > 100. {
+                    *PITCH_BEND.borrow(cs).borrow_mut() = new_freq ;
+                }
+            });
         }
     }
-
-    loop {}
 }
