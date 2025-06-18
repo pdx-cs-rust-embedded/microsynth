96de4807b03515399186c76e0f4530cb862dab37
Bart Massey <bart.massey@gmail.com>
2025-06-18 13:47:46 -0700

    upgraded to 2025

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,28 @@
 #![no_std]
 
 use core::cell::RefCell;
-use cortex_m::{asm, interrupt::Mutex};
+use cortex_m::interrupt::Mutex;
 use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
 
 use cortex_m_rt::entry;
+use embedded_hal::{digital::OutputPin, i2c::I2c};
 use microbit::hal::gpiote::Gpiote;
-use microbit::hal::prelude::_embedded_hal_blocking_i2c_WriteRead;
 use microbit::hal::twim;
 use microbit::pac::twim0::frequency::FREQUENCY_A;
 use microbit::{
     hal::{
         gpio,
-        prelude::OutputPin,
         pwm,
         rtc::{Rtc, RtcInterrupt},
         time::Hertz,
+        timer::Timer,
     },
     pac::{self, interrupt},
     Board,
 };
 
-use lsm303agr::{AccelOutputDataRate, Lsm303agr};
+use lsm303agr::{AccelMode, AccelOutputDataRate, Lsm303agr};
 
 const ACCELEROMETER_ADDR: u8 = 0b0011001;
 const ACCELEROMETER_ID_REG: u8 = 0x0f;
@@ -150,103 +150,108 @@ fn RTC0() {
 fn main() -> ! {
     rtt_init_print!();
 
-    if let Some(mut board) = Board::take() {
-        let gpiote = Gpiote::new(board.GPIOTE);
+    let mut board = Board::take().unwrap();
+    let gpiote = Gpiote::new(board.GPIOTE);
 
-        let channel0 = gpiote.channel0();
-        channel0
-            .input_pin(&board.buttons.button_a.degrade())
-            .hi_to_lo()
-            .enable_interrupt();
-        channel0.reset_events();
+    let channel0 = gpiote.channel0();
+    channel0
+        .input_pin(&board.buttons.button_a.degrade())
+        .hi_to_lo()
+        .enable_interrupt();
+    channel0.reset_events();
 
-        let channel1 = gpiote.channel1();
-        channel1
-            .input_pin(&board.buttons.button_b.degrade())
-            .hi_to_lo()
-            .enable_interrupt();
-        channel1.reset_events();
+    let channel1 = gpiote.channel1();
+    channel1
+        .input_pin(&board.buttons.button_b.degrade())
+        .hi_to_lo()
+        .enable_interrupt();
+    channel1.reset_events();
 
-        let mut i2c =
-            { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
+    let mut i2c =
+        { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
 
-        let mut acc = [0];
+    let mut acc = [0];
 
-        i2c.write_read(ACCELEROMETER_ADDR, &[ACCELEROMETER_ID_REG], &mut acc)
-            .unwrap();
+    i2c.write_read(ACCELEROMETER_ADDR, &[ACCELEROMETER_ID_REG], &mut acc)
+        .unwrap();
 
-        rprintln!("The accelerometer chip's id is: {:#b}", acc[0]);
+    rprintln!("The accelerometer chip's id is: {:#b}", acc[0]);
 
-        let mut sensor = Lsm303agr::new_with_i2c(i2c);
-        sensor.init().unwrap();
-        sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
+    let mut sensor = Lsm303agr::new_with_i2c(i2c);
+    let mut timer = Timer::new(board.TIMER0);
+    sensor.init().unwrap();
+    sensor.set_accel_mode_and_odr(
+        &mut timer,
+        AccelMode::Normal,
+        AccelOutputDataRate::Hz50,
+    ).unwrap();
 
-        cortex_m::interrupt::free(move |cs| {
-            /* Enable external GPIO interrupts */
-            unsafe {
-                pac::NVIC::unmask(pac::Interrupt::GPIOTE);
-            }
-            pac::NVIC::unpend(pac::Interrupt::GPIOTE);
-
-            *GPIO.borrow(cs).borrow_mut() = Some(gpiote);
-
-            let mut rtc = Rtc::new(board.RTC0, 511).unwrap();
-            rtc.enable_counter();
-            rtc.enable_interrupt(RtcInterrupt::Tick, Some(&mut board.NVIC));
-            rtc.enable_event(RtcInterrupt::Tick);
-
-            *RTC.borrow(cs).borrow_mut() = Some(rtc);
-
-            let mut speaker_pin = board.speaker_pin.into_push_pull_output(gpio::Level::High);
-            let _ = speaker_pin.set_low();
-
-            // Use the PWM peripheral to generate a waveform for the speaker
-            let speaker = pwm::Pwm::new(board.PWM0);
-            speaker
-                // output the waveform on the speaker pin
-                .set_output_pin(pwm::Channel::C0, speaker_pin.degrade())
-                // Use prescale by 16 to achive darker sounds
-                .set_prescaler(pwm::Prescaler::Div16)
-                // Initial frequency
-                .set_period(Hertz(440u32))
-                // Configure for up and down counter mode
-                .set_counter_mode(pwm::CounterMode::UpAndDown)
-                // Set maximum duty cycle
-                .set_max_duty(5000)
-                // enable PWM
-                .enable();
-
-            // Configure 50% duty cycle
-            let max_duty = speaker.max_duty();
-            speaker.set_duty_on_common(max_duty / 2);
-
-            *SPEAKER.borrow(cs).borrow_mut() = Some(speaker);
-
-            unsafe {
-                pac::NVIC::unmask(pac::Interrupt::RTC0);
-            }
-            pac::NVIC::unpend(pac::Interrupt::RTC0);
-        });
-
-        loop {
-            if sensor.accel_status().unwrap().xyz_new_data {
-                let data = sensor.accel_data().unwrap();
-
-                rprintln!("Acceleration: x {} y {} z {}", data.x, data.y, data.z);
-                cortex_m::interrupt::free(move |cs| {
-                    // Standard midi mapping ish
-                    let new_freq = data.x as f32 / 8192.0;
-                    let abs_data = libm::fabsf(data.x as f32);
-                    // Extremely scientific method to arrive at 100.
-                    if abs_data > 100. {
-                        *PITCH_BEND.borrow(cs).borrow_mut() = new_freq ;
-                    }
-                });
-            }
+    cortex_m::interrupt::free(move |cs| {
+        /* Enable external GPIO interrupts */
+        unsafe {
+            pac::NVIC::unmask(pac::Interrupt::GPIOTE);
         }
-    }
+        pac::NVIC::unpend(pac::Interrupt::GPIOTE);
+
+        *GPIO.borrow(cs).borrow_mut() = Some(gpiote);
+
+        let mut rtc = Rtc::new(board.RTC0, 511).unwrap();
+        rtc.enable_counter();
+        rtc.enable_interrupt(RtcInterrupt::Tick, Some(&mut board.NVIC));
+        rtc.enable_event(RtcInterrupt::Tick);
+
+        *RTC.borrow(cs).borrow_mut() = Some(rtc);
+
+        let mut speaker_pin = board.speaker_pin.into_push_pull_output(gpio::Level::High);
+        let _ = speaker_pin.set_low();
+
+        // Use the PWM peripheral to generate a waveform for the speaker
+        let speaker = pwm::Pwm::new(board.PWM0);
+        speaker
+            // output the waveform on the speaker pin
+            .set_output_pin(pwm::Channel::C0, speaker_pin.degrade())
+            // Use prescale by 16 to achive darker sounds
+            .set_prescaler(pwm::Prescaler::Div16)
+            // Initial frequency
+            .set_period(Hertz(440u32))
+            // Configure for up and down counter mode
+            .set_counter_mode(pwm::CounterMode::UpAndDown)
+            // Set maximum duty cycle
+            .set_max_duty(5000)
+            // enable PWM
+            .enable();
+
+        // Configure 50% duty cycle
+        let max_duty = speaker.max_duty();
+        speaker.set_duty_on_common(max_duty / 2);
+
+        *SPEAKER.borrow(cs).borrow_mut() = Some(speaker);
+
+        unsafe {
+            pac::NVIC::unmask(pac::Interrupt::RTC0);
+        }
+        pac::NVIC::unpend(pac::Interrupt::RTC0);
+    });
 
     loop {
-        asm::wfe();
+        if sensor.accel_status().unwrap().xyz_new_data() {
+            let data = sensor.acceleration().unwrap();
+
+            rprintln!(
+                "Acceleration: x {} y {} z {}",
+                data.x_mg(),
+                data.y_mg(),
+                data.z_mg(),
+            );
+            cortex_m::interrupt::free(move |cs| {
+                // Standard midi mapping ish
+                let new_freq = data.x_mg() as f32 / 8192.0;
+                let abs_data = libm::fabsf(data.x_mg() as f32);
+                // Extremely scientific method to arrive at 100.
+                if abs_data > 100. {
+                    *PITCH_BEND.borrow(cs).borrow_mut() = new_freq ;
+                }
+            });
+        }
     }
 }
